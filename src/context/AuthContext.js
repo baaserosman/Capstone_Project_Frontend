dec6327f4a993bbcc155e83c5e1a997c7850fff1
baaserosman <baaser.osman@gmail.com>
2022-06-25 00:55:57 +0200

    AuthContext

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,14 +1,14 @@
 import { createContext, useEffect, useState } from "react";
-import { userStateChecker } from "../auth/firebase";
+
 
 export const AuthContext = createContext();
 
 const AuthContextProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState();
 
-  useEffect(() => {
-    userStateChecker(setCurrentUser);
-  }, [currentUser]);
+  // useEffect(() => {
+  //   userStateChecker(setCurrentUser);
+  // }, [currentUser]);
 
   return (
     <AuthContext.Provider value={{ currentUser, setCurrentUser }}>
